498d1b62c15b53ba680be33c3ea67cd2eb16d8be
nagonad <nagonad@gmail.com>
2024-03-17 13:03:35 +0100

    added test cased for blog post published hepler motheds

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -2,13 +2,13 @@ class BlogPost < ApplicationRecord
   validates :title, presence:true
   validates :body, presence:true
 
-  scope :sorted, ->{order("published_at DESC NULLS LAST")}
+  scope :sorted, ->{order(published_at: :desc)}
   scope :draft,->{where(published_at:nil)}
   scope :published,->{where("published_at<=?",Time.current)}
   scope :scheduled,->{where("published_at>=?",Time.current)}
 
   def draft?
-    !published_at?
+    published_at.nil?
   end
   def published?
     published_at? && published_at <= Time.current
